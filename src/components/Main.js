16afcab41921e106b750919c5ba81f4b78f15171
brunnoL <brunnoL.@email.com>
2025-04-18 19:04:38 -0300

    :dizzy: CSS formulário

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,20 @@
 import React, { Component } from "react";
+
+// Formulário
+import { FaPlus } from 'react-icons/fa';
+// Tarefas
+import { FaEdit, FaWindowClose } from 'react-icons/fa';
+
 import './Main.css';
 
 export default class Main extends Component {
   state = {
     novaTarefa: '',
+    tarefas: [
+      'Fazer café',
+      'Beber água',
+      'Estudar',
+    ],
   }
 
   handleChange = (e) => {
@@ -13,15 +24,33 @@ export default class Main extends Component {
   }
 
   render() {
-    const { novaTarefa } = this.state;
+    const { novaTarefa, tarefas } = this.state;
     return (
       <div className="main">
         <h1>Lista de tarefas</h1>
 
-        <form action="#">
-          <input onChange={this.handleChange} type="text"></input>
-          <button type="submit">Enviar</button>
+        <form action="#" className="form">
+          <input onChange={this.handleChange}
+            type="text"
+            value={novaTarefa}
+          ></input>
+          <button type="submit">
+            <FaPlus />
+          </button>
         </form>
+
+        <ul className="tarefas">
+          {tarefas.map((tarefas) => (
+            <li key={tarefas}>
+              {tarefas}
+              <div>
+                <FaEdit className="edit" />
+                <FaWindowClose className="delete" />
+              </div>
+            </li>
+          ))}
+        </ul>
+
       </div>
     );
 
